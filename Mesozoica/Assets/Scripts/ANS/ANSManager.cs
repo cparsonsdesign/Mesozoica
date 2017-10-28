5c2389481266d946b1ee74f2a3a9e3d511a6f014
ChrisP <chris@cparsonsdesign.com>
2017-10-28 13:30:28 -0500

    added max node increases to ANS Manager upon the player reaching thresholds of the number of visitors the player has in the park

--- Mesozoica/Assets/Scripts/ANS/ANSManager.cs
+++ Mesozoica/Assets/Scripts/ANS/ANSManager.cs
@@ -10,14 +10,22 @@ using UnityEngine;
 
 public class ANSManager: MonoBehaviour, IGetRor
 {
+
     public List<Node> nodeList = new List<Node>();
+
+    // There needs to be a check against the max node number before a node is placed
     int nodeCount;
+    int nodeMax;
 
     public delegate void NewNode();
     public event NewNode NodePlaced;
 
     public delegate void LostNode();
     public event LostNode NodeRemoved;
+
+    int thresholdReached = 0;
+    // This is a temporary variable. This needs to be a reference to the player's actual visitor count.
+    public int visitorCount;
 
     static int SortByRor(Node node1, Node node2)
     {
@@ -31,9 +39,13 @@ public class ANSManager: MonoBehaviour, IGetRor
         return nodeList;
     }
 
+ 
     public void PlacedNode()
     {
-        NodePlaced();
+        if (NodePlaced != null)
+        {
+            NodePlaced();
+        }
         nodeCount = nodeList.Count;
     }
 
@@ -41,6 +53,17 @@ public class ANSManager: MonoBehaviour, IGetRor
     {
         nodeCount = nodeList.Count;
         NodeRemoved();
+    }
+
+    // This needs to be worked into the player's stats.
+   
+    public void ThresholdReached()
+    {
+        if(visitorCount >= thresholdReached + 100)
+        {
+            nodeMax += 3;
+            thresholdReached = thresholdReached + 100;
+        }
     }
 
 
